1c1cffb072c6f8f3652e7c3683cd25480b847402
Paco Grandez <jgrandec@emeal.nttdata.com>
2024-05-22 18:18:36 -0500

    pokemon interface creation - DONE!

---
+++ src/app/pokedex/commons/interfaces/pokemon.interface.ts
@@ -0,0 +1,45 @@
+export interface IBase {
+  name: string;
+  url: string;
+}
+
+export interface IPokemon {
+  id: string;
+  name: string;
+  abilities: IAbilitie[];
+  baseExperience: string;
+  locationAreaEncounters: string;
+  cries: ICries;
+  height: string;
+  weight: string;
+  types: IType[];
+  stats: IStats[];
+  image: string;
+}
+
+export interface IAbilitie {
+  ability: IBase;
+  isHidden: boolean;
+  slot: number;
+}
+
+export interface ICries {
+  latest: string;
+  legacy: string;
+}
+
+export interface IType {
+  slot: number;
+  type: IBase;
+}
+
+export interface IStats {
+  baseStat: number;
+  effort: number;
+  stat: IBase;
+}
+
+export interface IPagination<T> {
+  totalItems: number;
+  results: Array<T>;
+}
