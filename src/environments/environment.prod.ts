09fc344ec583a518a8c775a2d070eb182b40c73f
Paco Grandez <jgrandec@emeal.nttdata.com>
2024-05-22 18:17:16 -0500

    environment creation and config - DONE!

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const ENVIRONMENT = {
+  production: true,
+  apiUrl: 'https://pokeapi.co/api/v2/pokemon',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENVIRONMENT = {
+  production: false,
+  apiUrl: 'https://pokeapi.co/api/v2/pokemon',
+};
