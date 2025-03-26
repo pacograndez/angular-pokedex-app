d2b6794d43a07245de48f7409c314846614180ed
pacograndez <francisco.grandez@outlook>
2025-03-26 17:11:54 -0500

    feat: implement Pokedex component and update routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pokedex',
+    loadComponent: () => import('./pokedex/pokedex.component').then((m) => m.PokedexComponent)
+  },
+  { path: '', redirectTo: '/pokedex', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
